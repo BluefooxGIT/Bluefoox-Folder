b70ea6e0cd984cad4ff33629ce18806c036096ce
Bluefoox Admin <91162889+BluefooxGIT@users.noreply.github.com>
2022-04-28 21:00:33 -0500

    Update bf-ajustes.php

--- bf-interface/bf-ajustes.php
+++ bf-interface/bf-ajustes.php
@@ -190,34 +190,57 @@ $disco_espacio_usado_porcentaje = round( ( $disco_espacio_usado * 100 ) / $disco
     <span class="span-titulo-ajuste">Sistema</span>
     <div class="div-usuario-ajuste">
       <label class="label-titulos-ajuste">Versión estable actualizada.</label>
-      <?php $sha1_github = "https://github.com/BluefooxGIT/Bluefoox-Folder/archive/refs/heads/main.zip"; $sha1_github_nuevo = sha1_file($sha1_github); $sha1_actual = $row_root[ 'configuracion_sha1_actualizacion' ]; if($sha1_github_nuevo != $sha1_actual) { $github_actualizacion = $sha1_github; $github_actualizacion_nuevo = $_SERVER['DOCUMENT_ROOT'] . 'update_folder.zip'; if ( copy($github_actualizacion, $github_actualizacion_nuevo) ) { $zip = new ZipArchive; $res = $zip->open($github_actualizacion_nuevo); if ($res === TRUE) { $zip->extractTo(getcwd()); $zip->close(); function custom_copy($src, $dst) { $dir = opendir($src); @mkdir($dst); foreach (scandir($src) as $file) { if (( $file != '.' ) && ( $file != '..' )) { if ( is_dir($src . '/' . $file) ) { custom_copy($src . '/' . $file, $dst . '/' . $file); } else { copy($src . '/' . $file, $dst . '/' . $file); } } } closedir($dir); } $src = "Bluefoox-Folder-main"; $dst = getcwd(); custom_copy($src, $dst); mysqli_query( $mysqli, "UPDATE bluefoox_configuracion SET configuracion_sha1_actualizacion = '$sha1_github_nuevo' WHERE uid = '1'" ); } } } 
-	    
-$folderName = 'Bluefoox-Folder-main';
-removeFiles($folderName);
-function removeFiles($target) {
-	if(is_dir($target)){
-		$files = glob( $target . '*', GLOB_MARK );
+      <?php
+      $sha1_github = "https://github.com/BluefooxGIT/Bluefoox-Folder/archive/refs/heads/main.zip";
+      $sha1_github_nuevo = sha1_file( $sha1_github );
+      $sha1_actual = $row_root[ 'configuracion_sha1_actualizacion' ];
+      if ( $sha1_github_nuevo != $sha1_actual ) {
+        $github_actualizacion = $sha1_github;
+        $github_actualizacion_nuevo = $_SERVER[ 'DOCUMENT_ROOT' ] . 'update_folder.zip';
+        if ( copy( $github_actualizacion, $github_actualizacion_nuevo ) ) {
+          $zip = new ZipArchive;
+          $res = $zip->open( $github_actualizacion_nuevo );
+          if ( $res === TRUE ) {
+            $zip->extractTo( getcwd() );
+            $zip->close();
 
-  
-
-        foreach( $files as $file ){
-
-            removeFiles( $file );      
-
+            function custom_copy( $src, $dst ) {
+              $dir = opendir( $src );
+              @mkdir( $dst );
+              foreach ( scandir( $src ) as $file ) {
+                if ( ( $file != '.' ) && ( $file != '..' ) ) {
+                  if ( is_dir( $src . '/' . $file ) ) {
+                    custom_copy( $src . '/' . $file, $dst . '/' . $file );
+                  } else {
+                    copy( $src . '/' . $file, $dst . '/' . $file );
+                  }
+                }
+              }
+              closedir( $dir );
+            }
+            $src = "Bluefoox-Folder-main";
+            $dst = getcwd();
+            custom_copy( $src, $dst );
+            mysqli_query( $mysqli, "UPDATE bluefoox_configuracion SET configuracion_sha1_actualizacion = '$sha1_github_nuevo' WHERE uid = '1'" );
+          }
         }
+      }
+      $folderName = 'Bluefoox-Folder-main';
+      removeFiles( $folderName );
 
-  
-
-        rmdir( $target );
-
-    } elseif(is_file($target)) {
-
-        unlink( $target );  
-
-    }
-
-}
-	    ?>
+      function removeFiles( $target ) {
+        if ( is_dir( $target ) ) {
+          $files = glob( $target . '*', GLOB_MARK );
+          foreach ( $files as $file ) {
+            removeFiles( $file );
+          }
+          rmdir( $target );
+        } elseif ( is_file( $target ) ) {
+          unlink( $target );
+        }
+      }
+      unlink( '../bluefoox_documentos.sql' );
+      ?>
     </div>
   </div>
 </div>
@@ -301,4 +324,4 @@ $("#id-div-ajustes").click(function () {
 $("#id-div-cerrar-sesion").click(function () {
   location.href = 'bf-php/bf-cerrar-sesion.php';
 });
-</script>
+</script> 
