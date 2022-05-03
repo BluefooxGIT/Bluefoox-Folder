e9a18f69264a9fffa0bac04807133d449ee820cf
Bluefoox Admin <91162889+BluefooxGIT@users.noreply.github.com>
2022-05-03 13:25:28 -0500

    Add files via upload

--- bf-php/bf-filtro-carpeta.php
+++ bf-php/bf-filtro-carpeta.php
@@ -3,9 +3,8 @@ include_once( '../bf-php/bf-conexion.php' );
 $nombre_carpeta = $_POST[ 'nombre_carpeta' ];
 $direccion_carpetas = $_POST[ 'direccion_carpetas' ];
 if ( $nombre_carpeta != null ) {
-  $sql_filtro_carpeta = $mysqli->query( "SELECT * FROM bluefoox_archivos WHERE archivos_url = '$direccion_carpetas' AND archivos_nombre = '$nombre_carpeta' AND archivos_tipo = 'c'" );
+  $sql_filtro_carpeta = $mysqli->query( "SELECT * FROM bluefoox_archivos WHERE archivos_url = '$direccion_carpetas' AND archivos_nombre = '$nombre_carpeta_%' AND archivos_tipo = 'c'" );
   $row_filtro_carpeta = mysqli_fetch_array( $sql_filtro_carpeta );
-
   if ( isset( $row_filtro_carpeta ) ) {
     echo '<script>$("#id-boton-crear").hide(); $("#id-span-verificacion").show(); $("#id-span-verificacion").text("Este nombre ya existe"); var nombre_carpeta = $("#id-nombre-nueva-carpeta").val(); $("#id-nombre-nueva-carpeta").attr("placeholder", nombre_carpeta); $("#id-nombre-nueva-carpeta").val("");</script>';
   } else if ( !isset( $row_filtro_carpeta ) ) {
