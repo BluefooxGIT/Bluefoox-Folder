a2275490940f11ede288ddbf9feaf2d484ca7c5d
Bluefoox Admin <91162889+BluefooxGIT@users.noreply.github.com>
2022-05-03 04:19:11 -0500

    Update bf-principal.php

--- bf-interface/bf-principal.php
+++ bf-interface/bf-principal.php
@@ -306,7 +306,7 @@ function limpiar_busqueda() {
 
 $('#id-input-archivos').bind('change', function() {
     var tamano_archivos = (this.files[0].size);
-    if(tamano_archivos <= 8900000000){
+    if(tamano_archivos <= 500000000){
         almacenar_archivos()
     } else {
         $('#id-div-archivo-grande').show();
