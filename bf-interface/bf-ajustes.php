228a9a2f0b0582c1e917aa83a78681d979516089
Bluefoox Admin <91162889+BluefooxGIT@users.noreply.github.com>
2022-05-03 01:55:34 -0500

    Botón actualiza.
    Se ha actualizado la manera de actualizar el sistema.

--- bf-interface/bf-ajustes.php
+++ bf-interface/bf-ajustes.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(0);
+error_reporting( 0 );
 include_once( 'bf-php/bf-conexion.php' );
 if ( isset( $_SESSION[ 'usuarios_id' ] ) ) {
   $uid = $_SESSION[ 'usuarios_id' ];
@@ -196,56 +196,23 @@ $disco_espacio_usado_porcentaje = round( ( $disco_espacio_usado * 100 ) / $disco
       $sha1_github_nuevo = sha1_file( $sha1_github );
       $sha1_actual = $row_root[ 'configuracion_sha1_actualizacion' ];
       if ( $sha1_github_nuevo != $sha1_actual ) {
-        $github_actualizacion = $sha1_github;
-        $github_actualizacion_nuevo = $_SERVER[ 'DOCUMENT_ROOT' ] . 'update_folder.zip';
-        if ( copy( $github_actualizacion, $github_actualizacion_nuevo ) ) {
-          $zip = new ZipArchive;
-          $res = $zip->open( $github_actualizacion_nuevo );
-          if ( $res === TRUE ) {
-            $zip->extractTo( getcwd() );
-            $zip->close();
-
-            function custom_copy( $src, $dst ) {
-              $dir = opendir( $src );
-              @mkdir( $dst );
-              foreach ( scandir( $src ) as $file ) {
-                if ( ( $file != '.' ) && ( $file != '..' ) ) {
-                  if ( is_dir( $src . '/' . $file ) ) {
-                    custom_copy( $src . '/' . $file, $dst . '/' . $file );
-                  } else {
-                    copy( $src . '/' . $file, $dst . '/' . $file );
-                  }
-                }
-              }
-              closedir( $dir );
-            }
-            $src = "Bluefoox-Folder-main";
-            $dst = getcwd();
-            custom_copy( $src, $dst );
-            mysqli_query( $mysqli, "UPDATE bluefoox_configuracion SET configuracion_sha1_actualizacion = '$sha1_github_nuevo' WHERE uid = '1'" );
-          }
-        }
+        echo '<label class="label-titulos-ajuste">Existe una nueva actualizaci&oacute;n.</label>';
+        echo '<div class="div-actualizacion" onclick="actualizar_sistema()">Descargar y aplicar.</div>';
+      } else {
+        echo '<label class="label-titulos-ajuste">Versión estable actualizada.</label>';
       }
-      $folderName = 'Bluefoox-Folder-main';
-      removeFiles( $folderName );
-
-      function removeFiles( $target ) {
-        if ( is_dir( $target ) ) {
-          $files = glob( $target . '*', GLOB_MARK );
-          foreach ( $files as $file ) {
-            removeFiles( $file );
-          }
-          rmdir( $target );
-        } elseif ( is_file( $target ) ) {
-          unlink( $target );
-        }
-      }
-      unlink( 'bluefoox_documentos.sql' );
       ?>
+      <form id="id-form-actualizar" method="post" enctype="multipart/form-data" action="">
+      </form>
     </div>
   </div>
 </div>
 <script>
+function actualizar_sistema() {
+  $('#id-form-ajustes-llave').attr('action', 'bf-php/bf-ajustes-actualizar.php');
+  $('#id-form-ajustes-llave').submit();
+}
+	
 function generar_llave() {
   $('#id-form-ajustes-llave').attr('action', 'bf-php/bf-ajustes-generar-llave.php');
   $('#id-form-ajustes-llave').submit();
